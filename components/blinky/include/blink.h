e62e0652456a7f69b785952533b606ecb46850fb
Richard Kang <2356020+kangks@users.noreply.github.com>
2019-05-26 12:23:59 -0700

    clean up for esp32

---
+++ components/blinky/include/blink.h
@@ -0,0 +1,13 @@
+/*
+ * blink.h
+ *
+ *  Created on: May 26, 2019
+ *      Author: kangrich
+ */
+
+#ifndef COMPONENTS_BLINKY_BLINK_H_
+#define COMPONENTS_BLINKY_BLINK_H_
+
+void blinkTask(unsigned int);
+
+#endif /* COMPONENTS_BLINKY_BLINK_H_ */
